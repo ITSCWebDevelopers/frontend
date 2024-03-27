ebcb206baf65f5c4d140c31a88ddc9794e2832af
Camon-fi07 <422nikitos244@mail.ru>
2024-03-28 02:00:26 +0500

    temp

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,7 +1,15 @@
-import {createBrowserRouter} from 'react-router-dom';
+import {Link, createBrowserRouter} from 'react-router-dom';
 import {Login} from './pages/Login/Login';
+import {ROUTES} from './shared/constants/routes';
 
 export const ROUTER = createBrowserRouter([
-  {path: '/', element: <div>main</div>},
+  {
+    path: '/',
+    element: (
+      <div>
+        <Link to={ROUTES.LOGIN}>Login</Link>
+      </div>
+    ),
+  },
   {path: '/login', element: <Login />},
 ]);
