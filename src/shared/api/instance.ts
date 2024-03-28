38c44230f0a685938857206a3619a32083e140c2
Camon-fi07 <422nikitos244@mail.ru>
2024-03-28 15:48:00 +0500

    fix mocks

--- src/shared/api/instance.ts
+++ src/shared/api/instance.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const instance = axios.create({
-  baseURL: 'http://192.168.1.109:8000',
+  baseURL: 'http://localhost:31299/api',
   headers: {
     Accept: 'application/json',
     'Content-Type': 'application/json',
