00bdfc8f91530da3553f67ad10806cac72f156ed
Camon-fi07 <422nikitos244@mail.ru>
2024-03-28 16:28:03 +0500

    fix type

--- src/shared/api/interceptors/tokenInterceptor.ts
+++ src/shared/api/interceptors/tokenInterceptor.ts
@@ -1,9 +1,12 @@
-export const tokenInterceptor = (config: any) => {
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import type {InternalAxiosRequestConfig} from 'axios';
+
+export const tokenInterceptor = (value: InternalAxiosRequestConfig<any>) => {
   const token = localStorage.getItem('token');
 
-  if (token && config.headers) {
-    config.headers.Authorization = `Token ${token}`;
+  if (token && value.headers) {
+    value.headers.Authorization = `Token ${token}`;
   }
 
-  return config;
+  return value;
 };
