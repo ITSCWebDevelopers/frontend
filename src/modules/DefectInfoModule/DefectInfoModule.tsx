d93eacd1eace81ebe58723cc69b11881c4ab82b0
Aleksandr Osipov <otrijka04osip@gmail.com>
2024-03-29 09:42:59 +0500

    init page

---
+++ src/modules/DefectInfoModule/DefectInfoModule.tsx
@@ -0,0 +1,9 @@
+import {Box} from '@mui/material';
+
+export const DefectInfoModule = () => {
+  return (
+    <Box>
+      <>Модуль инфы о дефекте</>
+    </Box>
+  );
+};
---
+++ src/pages/DefectInfo/DefectInfo.tsx
@@ -0,0 +1,11 @@
+import {DefectInfoModule} from '@/modules/DefectInfoModule/DefectInfoModule';
+import {Box} from '@mui/material';
+import {styles} from './DefectInfo.styles';
+
+export const DefectInfo = () => {
+  return (
+    <Box sx={styles.wrapper}>
+      <DefectInfoModule />
+    </Box>
+  );
+};
